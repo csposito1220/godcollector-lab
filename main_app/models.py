aa7385bf8708ca37d67defe0aa17e75dbb33cd18
csposito1220 <csposito1220@gmail.com>
2023-11-21 11:01:57 -0500

    finish prayer model

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 # Create your models here.
+
+PRAYERS = (
+    ('P', 'Prosperity'),
+    ('F', 'Food'),
+    ('S', 'Safety'),
+)
 
 class God(models.Model):
     name = models.CharField()
@@ -24,3 +31,25 @@ class Mythology(models.Model):
 
     def get_absolute_url(self):
         return reverse('mythologies_detail', kwargs={'pk': self.id})
+    
+    def prayed_for_today(self):
+        return self.prayer_set.filter(date=date.today()).count() >= len(PRAYERS)
+
+class Prayer(models.Model):
+    date = models.DateField('Prayer Date')
+    prayer = models.CharField(
+        max_length=1,
+        choices=PRAYERS,
+        default=PRAYERS[0][0]
+    )
+
+    mythology = models.ForeignKey(
+        Mythology,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.get_prayer_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
